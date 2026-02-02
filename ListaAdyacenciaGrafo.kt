c685fb6fd6a3a87bc7f5c6f8a8c77fab452677eb
john3010gg <enriquegojohn@gmail.com>
2026-02-02 12:17:08 -0300

    Initial project structure: Interface and agregarVertice method

--- ListaAdyacenciaGrafo.kt
+++ ListaAdyacenciaGrafo.kt
@@ -1,4 +1,12 @@
 class ListaAdyacenciaGrafo<T> : Grafo<T> {
-    private val adyacencias: MutableMap<T, MutableList<T>> = MutableMapOf ()
+    private val adyacencias: MutableMap<T, MutableList<T>> = mutableMapOf () //El mapa donde vamos a guardar. Key = Vertice, Value = Lista de Sucesores
+
+    override fun agregarVertice(v: T): Boolean {
+        if (contiene(v)) return false
+        adyacencias[v] = mutableListOf() //Para iniciar el vertice con una lista vacia de sucesores
+        return true
+    }
+
+    
     
 }
